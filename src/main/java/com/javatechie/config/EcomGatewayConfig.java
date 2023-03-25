30ac1e7217d0979c2d63b04eb708e3b7c38ca63f
Basant kumar Hota <basahota@users.noreply.github.com>
2023-03-25 17:42:33 +0530

    circuit breaker impl

--- src/main/java/com/javatechie/config/EcomGatewayConfig.java
+++ src/main/java/com/javatechie/config/EcomGatewayConfig.java
@@ -8,23 +8,23 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class EcomGatewayConfig {
 
-    @Bean
-    public RouteLocator routeLocator(RouteLocatorBuilder builder) {
-
-        return builder.routes()
-                .route("order-service", r -> r.path("/orders/**")
-                        .and()
-                        .uri("lb://ORDER-SERVICE"))
-
-                .route("payment-service", r -> r.path("/payments/**")
-                        .and()
-                        .uri("lb://PAYMENT-SERVICE"))
-
-                .route("user-service", r -> r.path("/users/**")
-                        .and()
-                        .uri("lb://USER-SERVICE"))
-
-                .build();
-
-    }
+//    @Bean
+//    public RouteLocator routeLocator(RouteLocatorBuilder builder) {
+//
+//        return builder.routes()
+//                .route("order-service", r -> r.path("/orders/**")
+//                        .and()
+//                        .uri("lb://ORDER-SERVICE"))
+//
+//                .route("payment-service", r -> r.path("/payments/**")
+//                        .and()
+//                        .uri("lb://PAYMENT-SERVICE"))
+//
+//                .route("user-service", r -> r.path("/users/**")
+//                        .and()
+//                        .uri("lb://USER-SERVICE"))
+//
+//                .build();
+//
+//    }
 }
